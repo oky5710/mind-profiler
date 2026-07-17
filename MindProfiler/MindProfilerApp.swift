8172ed089b10e9d3ec9da1b0393b11b0681877e9
kyoh <kyoh@hutom.co.kr>
2026-07-17 20:44:17 +0900

    Initial Commit

---
+++ MindProfiler/MindProfilerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MindProfilerApp.swift
+//  MindProfiler
+//
+//  Created by 오기영 on 7/17/26.
+//
+
+import SwiftUI
+
+@main
+struct MindProfilerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
